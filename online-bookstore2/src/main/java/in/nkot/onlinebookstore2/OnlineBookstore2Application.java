b7b25c511669dd47611985730a729d2e9c1752e2
COCO <COCO@DESKTOP-Q583G4E>
2020-11-16 17:21:08 +0100

    Initial commit. created spring boot project and configure the application properties

---
+++ online-bookstore2/src/main/java/in/nkot/onlinebookstore2/OnlineBookstore2Application.java
@@ -0,0 +1,13 @@
+package in.nkot.onlinebookstore2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookstore2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookstore2Application.class, args);
+	}
+
+}
---
+++ online-bookstore2/src/test/java/in/nkot/onlinebookstore2/OnlineBookstore2ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nkot.onlinebookstore2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstore2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
